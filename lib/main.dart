236f094ba4100926b2e2ad47c57b2fcdf1e024bc
afnan923 <susumurni013@gmail.com>
2026-04-19 03:42:18 +0700

    fix:Main
    fix di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-
+import 'package:firebase_core/firebase_core.dart' as fb;
+import 'package:uts_1123150074/core/routes/app_router.dart';
+import 'package:uts_1123150074/core/theme/app_theme.dart';
+import 'package:uts_1123150074/features/auth/presentation/providers/auth_provider.dart';
+import 'package:uts_1123150074/features/dashboard/presentation/providers/product_provider.dart';
 import 'firebase_options.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   // 🔥 Inisialisasi Firebase
-  await Firebase.initializeApp(
+  await fb.Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
@@ -22,4 +23,25 @@ void main() async {
       child: const MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
+      child: MaterialApp(
+        title: 'My App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        initialRoute: AppRouter.splash,
+        routes: AppRouter.routes,
+      ),
+    );
+  }
 }
