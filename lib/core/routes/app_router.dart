dad27610e8e7a0abd780206f8ce6e3b5b8184d33
afnan923 <susumurni013@gmail.com>
2026-04-18 16:12:44 +0700

    feat:Core Constants & Services
    membuat app router untuk mengakses apk

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:uts_1123150074/features/auth/presentation/pages/login_page.dart';
+import 'package:uts_1123150074/features/auth/presentation/pages/register.page.dart';
+import 'package:uts_1123150074/features/auth/presentation/pages/verify_email.dart';
+import 'package:uts_1123150074/features/dashboard/presentation/pages/dashboard_page.dart';
+
+class AppRouter { 
+  static const String splash      = '/'; 
+  static const String login       = '/login'; 
+  static const String register    = '/register'; 
+  static const String verifyEmail = '/verify-email'; 
+  static const String dashboard   = '/dashboard'; 
+ 
+  static Map<String, WidgetBuilder> get routes => { 
+
+    login:       (_) => const LoginPage(), 
+    register:    (_) => const RegisterPage(), 
+    verifyEmail: (_) => const VerifyEmailPage(), 
+    dashboard:   (_) => const AuthGuard(child: DashboardPage()), 
+  }; 
+}
