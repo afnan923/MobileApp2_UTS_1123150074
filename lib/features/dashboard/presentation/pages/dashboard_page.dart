6034ca116d2d113a298a1d292b5a2e68053e0b99
afnan923 <susumurni013@gmail.com>
2026-04-19 03:38:10 +0700

    fix:Dashboard Layer
    memanggil route di dashboard page

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uts_1123150074/core/routes/app_router.dart';
 import 'package:uts_1123150074/features/auth/presentation/providers/auth_provider.dart';
 import 'package:uts_1123150074/features/dashboard/presentation/providers/product_provider.dart';
 
