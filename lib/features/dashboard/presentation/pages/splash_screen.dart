c2af90616888951a1f3cf0bbf196fc0ec5d50ec9
afnan923 <susumurni013@gmail.com>
2026-04-19 03:33:31 +0700

    feat:Dashboard Layer
    membuat splash screen

---
+++ lib/features/dashboard/presentation/pages/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:uts_1123150074/core/routes/app_router.dart';
+import 'package:uts_1123150074/core/services/secure_storage.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (!mounted) return;
+
+    final token = await SecureStorageService.getToken();
+
+    final route =
+        token != null ? AppRouter.dashboard : AppRouter.login;
+
+    Navigator.pushReplacementNamed(context, route);
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+}
