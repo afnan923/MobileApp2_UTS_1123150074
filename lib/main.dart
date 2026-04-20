0f961ce354147ee1875d6f3a1ed72332aab11c5b
afnan923 <susumurni013@gmail.com>
2026-04-20 17:57:15 +0700

    fix: Main
    tambah bagian provider cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart' as fb;
+
 import 'package:uts_1123150074/core/routes/app_router.dart';
 import 'package:uts_1123150074/core/theme/app_theme.dart';
+
 import 'package:uts_1123150074/features/auth/presentation/providers/auth_provider.dart';
 import 'package:uts_1123150074/features/dashboard/presentation/providers/product_provider.dart';
-import 'firebase_options.dart';
 
+// 🛒 CART IMPORT
+import 'package:uts_1123150074/features/cart/presentation/providers/cart_provider.dart';
+import 'package:uts_1123150074/features/cart/data/repositories/cart_repository_impl.dart';
+import 'package:uts_1123150074/features/cart/data/datasources/cart_remote_datasource.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +27,10 @@ void main() async {
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => ProductProvider()),
+        ChangeNotifierProvider<CartProvider>(
+          create: (context) =>
+              CartProvider(CartRepositoryImpl(CartRemoteDataSource())),
+        ),
       ],
       child: const MyApp(),
     ),
@@ -32,7 +43,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
+      title: 'Paket Alat Pancing',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       initialRoute: AppRouter.splash,
