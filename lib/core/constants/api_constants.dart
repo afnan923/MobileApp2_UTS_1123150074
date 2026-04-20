d557905691fc2d3d0d6fbed1904f0fdd8e7716d5
afnan923 <susumurni013@gmail.com>
2026-04-20 15:48:54 +0700

    feat: Core & Constants
    menambah bagian cart dan orders di api_constants

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,13 +1,23 @@
-class ApiConstants { 
-  static const String baseUrl = 'http://172.20.10.6:8080/v1'; 
-  
-  // Auth endpoints 
-  static const String verifyToken = '/auth/verify-token'; 
-  
-  // Product endpoints 
-  static const String products = '/products'; 
-  
-  // Timeout 
-  static const int connectTimeout = 15000; 
-  static const int receiveTimeout = 15000; 
+class ApiConstants {
+  static const String baseUrl = 'http://172.20.10.6:8080/v1';
+
+  // Auth
+  static const String verifyToken = '/auth/verify-token';
+
+  // Products
+  static const String products = '/products';
+
+  // Cart
+  static const String cart = '/cart';
+
+  // Orders
+  static const String orders = '/orders';
+  static const String checkout = '/orders/checkout';
+
+  // Admin Orders
+  static const String adminOrders = '/admin/orders';
+
+  // Timeout
+  static const int connectTimeout = 15000;
+  static const int receiveTimeout = 15000;
 }
