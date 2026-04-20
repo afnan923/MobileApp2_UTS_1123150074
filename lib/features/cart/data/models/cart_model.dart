6eef73cee57fc3ae3958b6859ae0e20eca1d9bdb
afnan923 <susumurni013@gmail.com>
2026-04-20 16:42:36 +0700

    fix: Cart
    ganti code cart_model

--- lib/features/cart/data/models/cart_model.dart
+++ lib/features/cart/data/models/cart_model.dart
@@ -1,15 +1,21 @@
-class CartItemEntity {
-  final int id;
-  final int productId;
-  final String productName;
-  final int price;
-  final int quantity;
+import '../../domain/entities/cart_entity.dart';
 
-  CartItemEntity({
-    required this.id,
-    required this.productId,
-    required this.productName,
-    required this.price,
-    required this.quantity,
+class CartItemModel extends CartItemEntity {
+  CartItemModel({
+    required super.id,
+    required super.productId,
+    required super.productName,
+    required super.price,
+    required super.quantity,
   });
+
+  factory CartItemModel.fromJson(Map<String, dynamic> json) {
+    return CartItemModel(
+      id: json['id'],
+      productId: json['product_id'],
+      productName: json['product_name'] ?? '-',
+      price: json['price'],
+      quantity: json['quantity'],
+    );
+  }
 }
