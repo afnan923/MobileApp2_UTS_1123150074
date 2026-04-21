18aa42998296bd286692c86147e7c615ce0945c0
afnan923 <susumurni013@gmail.com>
2026-04-21 21:19:49 +0700

    fix: Core Constants & Services
    add file notif

---
+++ lib/core/services/notification_service.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin _notifications =
+      FlutterLocalNotificationsPlugin();
+
+  static Future<void> init() async {
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const settings = InitializationSettings(
+      android: android,
+    );
+
+    await _notifications.initialize(settings);
+  }
+
+  static Future<void> showNotification({
+    required String title,
+    required String body,
+  }) async {
+    const androidDetails = AndroidNotificationDetails(
+      'main_channel',
+      'Main Notifications',
+      channelDescription: 'Notifikasi utama aplikasi',
+      importance: Importance.high,
+      priority: Priority.high,
+    );
+
+    const details = NotificationDetails(android: androidDetails);
+
+    await _notifications.show(
+      0,
+      title,
+      body,
+      details,
+    );
+  }
+}
