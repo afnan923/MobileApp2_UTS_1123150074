12bb995a9fa4ce7c75a86a6854cb802620209808
afnan923 <susumurni013@gmail.com>
2026-04-21 21:21:04 +0700

    fix: Main
    call notif

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart' as fb;
 import 'package:uts_1123150074/core/routes/app_router.dart';
+import 'package:uts_1123150074/core/services/notification_service.dart';
 import 'package:uts_1123150074/core/theme/app_theme.dart';
 import 'package:uts_1123150074/features/auth/presentation/providers/auth_provider.dart';
 import 'package:uts_1123150074/features/dashboard/presentation/providers/product_provider.dart';
@@ -18,7 +19,7 @@ void main() async {
   await fb.Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  await NotificationService.init();
   runApp(
     MultiProvider(
       providers: [
