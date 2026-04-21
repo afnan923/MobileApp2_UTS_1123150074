dbf79b1180e145f2dedb6beb8305f2880b4d101e
afnan923 <susumurni013@gmail.com>
2026-04-21 21:15:09 +0700

    fix: Main
    fix di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart' as fb;
-
 import 'package:uts_1123150074/core/routes/app_router.dart';
 import 'package:uts_1123150074/core/theme/app_theme.dart';
-
 import 'package:uts_1123150074/features/auth/presentation/providers/auth_provider.dart';
 import 'package:uts_1123150074/features/dashboard/presentation/providers/product_provider.dart';
-
-// 🛒 CART IMPORT
+// CART IMPORT
 import 'package:uts_1123150074/features/cart/presentation/providers/cart_provider.dart';
 import 'package:uts_1123150074/features/cart/data/repositories/cart_repository_impl.dart';
 import 'package:uts_1123150074/features/cart/data/datasources/cart_remote_datasource.dart';
