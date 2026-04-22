afcf9e36769252a11e8870e62d6d4cee0116f2eb
afnan923 <susumurni013@gmail.com>
2026-04-22 19:20:37 +0700

    feat: Dashboard Pages
    menambahkan product repository;

---
+++ lib/features/dashboard/domain/repositories/product_repository.dart
@@ -0,0 +1,6 @@
+import 'package:uts_1123150074/features/dashboard/data/models/product_model.dart';
+
+abstract class ProductRepository {
+  Future<List<ProductModel>> getProducts({int page = 1, int limit = 10, String? category});
+  Future<ProductModel> getProductById(int id);
+}
